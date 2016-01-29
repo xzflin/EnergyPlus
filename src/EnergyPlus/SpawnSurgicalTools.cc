45bc1cb8a22eeba8a7ecc1ddcef15944d9914eb3
Edwin Lee <leeed2001@gmail.com>
2016-01-29 09:59:06 -0700

    Interim commit, EMS isnt working and not sure why...

--- src/EnergyPlus/SpawnSurgicalTools.cc
+++ src/EnergyPlus/SpawnSurgicalTools.cc
@@ -1,16 +1,25 @@
 #include <DataPrecisionGlobals.hh>
+#include <EMSManager.hh>
+#include <EnergyPlus.hh>
 #include <OutputProcessor.hh>
 #include <SpawnSurgicalTools.hh>
 
 namespace SpawnSurgicalTools {
-	Real64 reportingValue; // this is just the integrated value
-	Real64 derivative; // this is the current derivative
+
+	// Ugly namespace level variables to expedite the process here
+	Real64 reportingValue( 0.0 ); // this is just the integrated value
+	Real64 derivative( 0.0 ); // this is the current derivative
+	bool EMSActive( false );
+	Real64 EMSTimeDerivative( 0.0 );
+
+	// Integration function
 	Real64 integrateDummyODE( Real64 initialValue, Real64 timestep )
 	{
 		static bool oneTimeFlag( true );
 		if ( oneTimeFlag ) {
-			EnergyPlus::SetupOutputVariable( "Spawn Surgery Dummy Integrated Value[]", reportingValue, "System", "Average", "Edwin" );
-			EnergyPlus::SetupOutputVariable( "Spawn Surgery Dummy Time Derivative[]", derivative, "System", "Average", "Edwin" );
+			EnergyPlus::SetupOutputVariable( "SPAWN SURGERY DUMMY INTEGRATED VALUE [J]", reportingValue, "SYSTEM", "AVERAGE", "EDWIN" );
+			EnergyPlus::SetupOutputVariable( "SPAWN SURGERY DUMMY TIME DERIVATIVE [W]", derivative, "SYSTEM", "AVERAGE", "EDWIN" );
+			EnergyPlus::SetupEMSActuator( "BRANCH", "EDWIN", "TIME DERIVATIVE", "[W]", EMSActive, EMSTimeDerivative );
 			oneTimeFlag = false;
 		}
 		// euler integration
@@ -19,10 +28,16 @@ namespace SpawnSurgicalTools {
 		return reportingValue;
 	}
 
+	// Time derivative function
 	Real64 getTimeDerivative( Real64 initialValue )
 	{
-		Real64 A = 0.0005;
-		Real64 B = 4;
-		return A * initialValue + B;
+		if ( EMSActive ) {
+			return EMSTimeDerivative;
+		} else {
+			Real64 A = 0.0005;
+			Real64 B = 4;
+			return A * initialValue + B;
+		}
 	}
+
 }
